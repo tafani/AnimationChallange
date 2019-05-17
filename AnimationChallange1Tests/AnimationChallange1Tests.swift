9a74113a92f454d1ffaa788c7f9528a8f840528c
muhammadtafanirabbani <ratafani@gmail.com>
2019-05-17 16:05:44 +0700

    Initial Commit

---
+++ AnimationChallange1Tests/AnimationChallange1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  AnimationChallange1Tests.swift
+//  AnimationChallange1Tests
+//
+//  Created by Muhammad Tafani Rabbani on 14/05/19.
+//  Copyright © 2019 Muhammad Tafani Rabbani. All rights reserved.
+//
+
+import XCTest
+@testable import AnimationChallange1
+
+class AnimationChallange1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
